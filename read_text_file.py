73c12886284e963bad045673fb3811d83cfaea8e
andre <andre.scholich@gmail.com>
2019-01-19 14:30:10 +0100

    Include reading from data in the example file

--- read_text_file.py
+++ read_text_file.py
@@ -1,5 +1,5 @@
 from pylab import np
 
-with open('testfile.txt') as f:
+with open('ML Sample Data/RL 1k/TIME/Gaussian F=0.400 DC=50 A=0.50 TIME [2019-01-18 13_58_52.56].csv') as f:
     arr = np.array([float(item) for item in f.readlines()])
     print(arr)
